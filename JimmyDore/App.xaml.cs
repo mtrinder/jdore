3ea63dc6be187c7e3687c0566c21e560aa9e7dfa
Mark Trinder <mtrinder@orbital8.com.au>
2021-07-07 21:20:34 +1000

    Push Message dialogs

--- JimmyDore/App.xaml.cs
+++ JimmyDore/App.xaml.cs
@@ -72,28 +72,10 @@ namespace JimmyDore
 #if DEBUG
                 System.Diagnostics.Debug.WriteLine("Received");
 #endif
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    PlayBells();
-
-                    //IJimmyDoreDialogService DialogService;
-                    //DialogService = Container.Resolve<IJimmyDoreDialogService>();
-                    //await DialogService.DisplayAlertWithOk("The Jimmy Dore Show", "We are now live on YouTube!");
-                });
-
-                
+                Device.BeginInvokeOnMainThread(() => PlayBells());
             };
 
-            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
-            {
-#if DEBUG
-                System.Diagnostics.Debug.WriteLine("Opened");
-                foreach (var data in p.Data)
-                {
-                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
-                }
-#endif
-            };
+            CrossFirebasePushNotification.Current.OnNotificationOpened += Current_OnNotificationOpened;
 
             CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
             {
@@ -110,6 +92,81 @@ namespace JimmyDore
                 }
 #endif
             };
+        }
+
+        DateTime dateTime;
+
+        private void Current_OnNotificationOpened(object source, FirebasePushNotificationResponseEventArgs p)
+        {
+            System.Diagnostics.Debug.WriteLine("Opened");
+
+            var liveNow = false;
+            var liveShow = false;
+
+            if (dateTime != default && (DateTime.Now - dateTime).TotalMilliseconds < TimeSpan.FromSeconds(30).TotalMilliseconds)
+            {
+                return;
+            }
+
+            dateTime = DateTime.Now;
+
+            foreach (var data in p.Data)
+            {
+                if (data.Key.Equals("google.c.a.c_l") && data.Value.Equals("Live Show"))
+                {
+                    liveShow = true;
+                }
+                if (data.Key.Equals("google.c.a.c_l") && data.Value.Equals("Live Now"))
+                {
+                    liveNow = true;
+                }
+
+                if (data.Key.Equals("aps.alert.body") && liveNow)
+                {
+                    liveNow = false;
+
+                    Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+                    {
+                        Device.InvokeOnMainThreadAsync(async () =>
+                        {
+                            var dialogs = Container.Resolve<IJimmyDoreDialogService>();
+                            var answer = await dialogs.UserAcceptedDisplayAlert("Live Show Starting Soon!", $"Open in YouTube now?", "Yes", "No");
+                            if (answer)
+                            {
+                                await Xamarin.Essentials.Launcher.OpenAsync("https://www.youtube.com/channel/UC3M7l8ved_rYQ45AVzS0RGA");
+                            }
+                        });
+
+                        return false;
+                    });
+                }
+
+                if (data.Key.Equals("aps.alert.body") && data.Value.ToString().Contains("https") && liveShow)
+                {
+                    liveShow = false;
+
+                    var parts = data.Value.ToString().Split(new[] { "https" }, StringSplitOptions.None);
+
+                    var link = $"https{parts[1]}";
+
+                    Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+                    {
+                        Device.InvokeOnMainThreadAsync(async () =>
+                        {
+                            var dialogs = Container.Resolve<IJimmyDoreDialogService>();
+                            var answer = await dialogs.UserAcceptedDisplayAlert("New Live Event Coming!", $"Check it out now?\n\n{link}", "Yes", "No");
+                            if (answer)
+                            {
+                                await Xamarin.Essentials.Launcher.OpenAsync(link);
+                            }
+                        });
+
+                        return false;
+                    });
+                }
+
+                System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+            }
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
